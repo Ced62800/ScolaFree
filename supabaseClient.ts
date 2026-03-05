eb83f6cde6e08974758a58f325a7c8741fc4e507
Ced62800 <cedric.desgardin@gmail.com>
2026-03-05 14:45:14 +0100

    fix supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -5,5 +5,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Pas de throw ici : Vercel ne peut pas injecter les variables pendant le build
 export const supabase = createClient(supabaseUrl ?? "", supabaseKey ?? "");
